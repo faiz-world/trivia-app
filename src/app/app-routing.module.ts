fa91dc3a038aa9ae9caf17c96f8b139e89e2f695
faiz-world <faizhussain007@gmail.com>
2019-07-12 16:07:36 +0530

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { HistoryComponent } from './history/history.component'
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'history', component: HistoryComponent },
+  { path: '**', redirectTo: '', pathMatch: 'full' }
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
